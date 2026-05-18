cce2106730a244d796ae2786a6984896a20fbf63
PeterTimCousins <petertimcousins@gmail.com>
2026-05-18 13:14:04 +0100

    Balance message pane gutters

--- Sources/SynologyChatNative/Theme.swift
+++ Sources/SynologyChatNative/Theme.swift
@@ -238,7 +238,7 @@ html[data-scn-theme="modernDark"] body.syno-chat .msg-title .channel-star {
 html[data-scn-theme="modernDark"] body.syno-chat .item-wrap {
   background: transparent !important;
   border-radius: 10px !important;
-  margin-left: 14px !important;
+  margin-left: 4px !important;
   margin-right: 14px !important;
 }
 
@@ -295,11 +295,11 @@ html[data-scn-theme="modernDark"] body.syno-chat .extra-date-splitter-wrapper {
   background: #121b27 !important;
   box-sizing: border-box !important;
   left: 0 !important;
-  margin-left: 14px !important;
+  margin-left: 4px !important;
   margin-right: 14px !important;
   padding-left: 0 !important;
   padding-right: 0 !important;
-  width: calc(100% - 28px) !important;
+  width: calc(100% - 18px) !important;
 }
 
 html[data-scn-theme="modernDark"] body.syno-chat .extra-date-splitter-wrapper .date-splitter-line {
